dba105a66734647cef84736f45fb7882c2f3d5c7
Touré Holder <toureh@hotmail.com>
2020-06-25 20:01:12 -0300

    fix: something

--- packages/poc-wiz-button/src/WizButton.ts
+++ packages/poc-wiz-button/src/WizButton.ts
@@ -1,32 +1,34 @@
-import { html, css, LitElement, property } from 'lit-element';
-import { LionButton } from '@lion/button';
+import { html, css, LitElement, property } from "lit-element";
+import { LionButton } from "@lion/button";
 
 export class WizButton extends LionButton {
   static get styles() {
+    console.log("patch");
+
     return [
       super.styles,
       css`
         .btn {
-            background: var(--wiz-button-primary, rgb(245, 108, 0));
-            color: rgb(255, 255, 255);
-            font-family: 'Roboto', sans-serif;
-            font-size: small;
-            border-radius: 5px;
+          background: var(--wiz-button-primary, rgb(245, 108, 0));
+          color: rgb(255, 255, 255);
+          font-family: "Roboto", sans-serif;
+          font-size: small;
+          border-radius: 5px;
         }
 
         :host(:hover) .btn {
-            background: var(--wiz-button-background-hover, rgb(255, 145, 0));
+          background: var(--wiz-button-background-hover, rgb(255, 145, 0));
         }
 
         :host(:focus:not([disabled])) .btn {
-            outline: none;
-          }
+          outline: none;
+        }
 
         :host(:active) .btn, /* keep native :active to render quickly where possible */
         :host([active]) .btn /* use custom [active] to fix IE11 */ {
-            background: var(--wiz-button-background-clicked, rgb(255, 173, 66));
+          background: var(--wiz-button-background-clicked, rgb(255, 173, 66));
         }
-    `
+      `,
     ];
   }
 }
